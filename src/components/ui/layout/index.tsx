876247dc923bf7727de09e7163e138fb477effa2
MrRobz <robinphilipthomas@outlook.in>
2021-07-15 23:53:10 +0530

    add tailwind config

--- src/components/ui/layout/index.tsx
+++ src/components/ui/layout/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
 
 export const Layout: FC = ({ children }) => {
-  return <div className="flex w-full h-full">{children}</div>;
+  return <div className="flex w-full h-full bg-black1">{children}</div>;
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@ module.exports = {
   purge: ['./src/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+
+        highlight: '#FF5930',
+        white: '#FFFFFF',
+        body: '#777F93',
+        primary: '#7230FF',
+        black1: '#0D1118',
+        black2: '#141C25',
+
+        gradientCoolMint: 'linear-gradient(180deg, #696EFF 0%, #BFEEE8 100%)',
+        gradientGrapeGum: 'linear-gradient(180deg, #696EFF 0%, #F7ABFF 100%)',
+      },
+      fontSize: {
+        xs: ['0.625rem', { lineHeight: '1rem' }],
+        sm: ['0.75rem', { lineHeight: '1rem' }],
+        base: ['0.875rem', { lineHeight: '1.25rem' }],
+        lg: ['1rem', { lineHeight: '1.5rem' }],
+        xl: ['1.125rem', { lineHeight: '1.75rem' }],
+        '2xl': ['1.25rem', { lineHeight: '1.75rem' }],
+        '3xl': ['1.5rem', { lineHeight: '2rem' }],
+        '4xl': ['1.875rem', { lineHeight: '2.25rem' }],
+        '5xl': ['2.25rem', { lineHeight: '2.5rem' }],
+        '6xl': ['3rem', { lineHeight: '1' }],
+        '7xl': ['3.75rem', { lineHeight: '1' }],
+        '8xl': ['4.5rem', { lineHeight: '1' }],
+        '9xl': ['6rem', { lineHeight: '1' }],
+      },
+    },
   },
   variants: {
     extend: {},
