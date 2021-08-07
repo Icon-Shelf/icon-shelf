c1e9d18e4fed4ecb0dc416b068ee90527d625dc2
MrRobz <robinphilipthomas@outlook.in>
2021-08-07 15:20:59 +0530

    update font to dm-sans

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.global.css';
 import IconsHome from './components/modules/IconsHome';
 import { Layout } from './components/ui/layout/index';
+import '@fontsource/dm-sans';
 
 export default function App() {
   return (
