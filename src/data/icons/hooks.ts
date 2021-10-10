a16cd6317bc4924b4685b9c24da8e446f006a727
MrRobz <robinphilipthomas@outlook.in>
2021-10-10 23:13:56 +0530

    typescript fix

--- src/data/icons/hooks.ts
+++ src/data/icons/hooks.ts
@@ -2,12 +2,17 @@ import { checkIfAnyNewIconsInFolder } from 'data/icons';
 import { useEffect } from 'react';
 import { useQueryClient } from 'react-query';
 
+declare function requestIdleCallback(
+  callback: () => void,
+  options?: { timeout: number }
+): number;
+
 export const useCheckIfAnyNewIconsInFolder = (collectionId: string) => {
   const queryClient = useQueryClient();
 
   useEffect(() => {
     if (collectionId && parseInt(collectionId)) {
-      window.requestIdleCallback(
+      requestIdleCallback(
         () => checkIfAnyNewIconsInFolder(collectionId, queryClient),
         { timeout: 3000 }
       );
