11319779e57d96a85aee343e6fdc9b5254945e49
MrRobz <robinphilipthomas@outlook.in>
2021-10-19 01:07:08 +0530

    codemirror basic setup

--- src/components/modules/IconsHome/LeftIconsCollectionsNav/CustomizeActionsModal/EditActionSection/TemplateStringCreator/index.tsx
+++ src/components/modules/IconsHome/LeftIconsCollectionsNav/CustomizeActionsModal/EditActionSection/TemplateStringCreator/index.tsx
@@ -1,5 +1,6 @@
 import { TextArea } from 'components/ui/atomic-components';
 import { ChangeEvent, FC } from 'react';
+import { TemplateEditor } from './TemplateEditor';
 
 interface Props {
   defaultValue: string;
@@ -21,7 +22,15 @@ export const TemplateStringCreator: FC<Props> = ({
       <label className="flex font-medium text-gray-400 ml-1 mb-1">
         Copy string template
       </label>
-      <TextArea defaultValue={defaultValue} onChange={onTextChange} />
+      <TemplateEditor
+        value={defaultValue}
+        onUpdate={(v) => {
+          if (v.docChanged) {
+            console.log(v.state.doc.toString());
+          }
+        }}
+      />
+      {/* <TextArea defaultValue={defaultValue} onChange={onTextChange} /> */}
     </div>
   );
 };
