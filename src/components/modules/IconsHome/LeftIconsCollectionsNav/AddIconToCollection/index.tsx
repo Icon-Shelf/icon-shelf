40fcf83feaf350f2028baae95c26a8896e41d349
Robin Philip Thomas <21201812+MrRobz@users.noreply.github.com>
2021-10-25 23:18:59 +0530

    feat: open "Add icons to collection" when an icon in dragged to window (#32)

--- src/components/modules/IconsHome/LeftIconsCollectionsNav/AddIconToCollection/index.tsx
+++ src/components/modules/IconsHome/LeftIconsCollectionsNav/AddIconToCollection/index.tsx
@@ -1,10 +1,24 @@
-import { FC, useState } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
 import { Button } from 'components/ui/atomic-components';
 import { ReactComponent as PlusIcon } from 'assets/icons/plus.svg';
 import { AddIconToCollectionModal } from './modal';
 
 export const AddIconToCollection: FC = () => {
   const [showIconAddModal, setShowIconAddModal] = useState(false);
+
+  const handleDragStart = useCallback((event: Event) => {
+    setShowIconAddModal(true);
+    event.preventDefault();
+  }, []);
+
+  useEffect(() => {
+    const dom = document.querySelector('#root');
+    dom?.addEventListener('dragover', handleDragStart);
+
+    return () => {
+      dom?.removeEventListener('dragover', handleDragStart);
+    };
+  }, [handleDragStart]);
 
   return (
     <>
