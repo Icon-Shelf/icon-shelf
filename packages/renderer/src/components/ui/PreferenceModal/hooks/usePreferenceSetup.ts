6175cc181c4f7d553272a88c34029e233a8b878d
MrRobz <robinphilipthomas@outlook.in>
2022-07-13 22:15:44 +0530

    lint fix

--- packages/renderer/src/components/ui/PreferenceModal/hooks/usePreferenceSetup.ts
+++ packages/renderer/src/components/ui/PreferenceModal/hooks/usePreferenceSetup.ts
@@ -16,7 +16,7 @@ export const usePreferenceModalSetup = () => {
         onOpenPreferencesCallback
       );
     };
-  }, []);
+  }, [onOpenPreferencesCallback]);
 
   return {
     isPreferenceModalVisible,
--- packages/renderer/src/components/ui/PreferenceModal/svgo-default-preference.ts
+++ packages/renderer/src/components/ui/PreferenceModal/svgo-default-preference.ts
@@ -3,6 +3,7 @@ export interface SvgoSettingItem {
   desc: string;
   active: boolean;
   isAux?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   params?: any;
 }
 
