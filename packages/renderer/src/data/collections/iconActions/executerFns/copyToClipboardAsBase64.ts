6d1ce94e81359be6e9c63a3557dd9285f0405f97
parnus01 <danusorn.t@ku.th>
2022-10-03 16:48:18 +0700

    fix(clean-up) : add missing await

--- packages/renderer/src/data/collections/iconActions/executerFns/copyToClipboardAsBase64.ts
+++ packages/renderer/src/data/collections/iconActions/executerFns/copyToClipboardAsBase64.ts
@@ -3,5 +3,5 @@ import type { ExecuterProps } from '../useOnActionClick';
 export const copyToClipboardAsBase64 = async ({ icon }: ExecuterProps) => {
   const iconContents = window.electron.ipcRenderer.sendSync('get-icon-file-content', icon.imageSrc);
   const copyText = window.btoa(iconContents)
-  navigator.clipboard.writeText(copyText);
+  await navigator.clipboard.writeText(copyText);
 };
--- packages/renderer/src/data/collections/iconActions/executerFns/copyToClipboardAsSvg.ts
+++ packages/renderer/src/data/collections/iconActions/executerFns/copyToClipboardAsSvg.ts
@@ -2,5 +2,5 @@ import type { ExecuterProps } from '../useOnActionClick';
 
 export const copyToClipboardAsSvg = async ({ icon }: ExecuterProps) => {
   const iconContents = window.electron.ipcRenderer.sendSync('get-icon-file-content', icon.imageSrc);
-  navigator.clipboard.writeText(iconContents);
+  await navigator.clipboard.writeText(iconContents);
 };
