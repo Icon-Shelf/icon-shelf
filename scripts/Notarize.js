09dcafbe807559a9d5dcacbe9da5513b382b3b2c
MrRobz <robinphilipthomas@outlook.in>
2023-11-04 17:32:27 +0530

    update

--- scripts/Notarize.js
+++ scripts/Notarize.js
@@ -7,15 +7,15 @@ exports.default = async function notarizeMacos(context) {
     return;
   }
 
-  if (!process.env.CI) {
-    console.warn('Skipping notarizing step. Packaging is not running in CI');
-    return;
-  }
+  // if (!process.env.CI) {
+  //   console.warn('Skipping notarizing step. Packaging is not running in CI');
+  //   return;
+  // }
 
-  if (!('APPLE_ID' in process.env && 'APPLE_ID_PASS' in process.env)) {
-    console.warn('Skipping notarizing step. APPLE_ID and APPLE_ID_PASS env variables must be set');
-    return;
-  }
+  // if (!('APPLE_ID' in process.env && 'APPLE_APP_SPECIFIC_PASSWORD' in process.env)) {
+  //   console.warn('Skipping notarizing step. APPLE_ID and APPLE_APP_SPECIFIC_PASSWORD env variables must be set');
+  //   return;
+  // }
 
   const appName = context.packager.appInfo.productFilename;
 
@@ -23,6 +23,6 @@ exports.default = async function notarizeMacos(context) {
     appBundleId: electronBuilderConfig.appId,
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_ID_PASS,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
   });
 };
